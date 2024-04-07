4b04b20e5f8705f7480fb721a605295b428600d2
Nicola3423 <147004823+Nicola3423@users.noreply.github.com>
2024-04-06 21:06:37 -0300

    Update ConexaoDB.java

--- sprint_sales_api/src/main/java/connection/ConexaoDB.java
+++ sprint_sales_api/src/main/java/connection/ConexaoDB.java
@@ -13,7 +13,7 @@ public class ConexaoDB {
        
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
-            conn = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "rm554227", "081204");
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "rm554227", "");
            
         } catch (SQLException e) {
             System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
